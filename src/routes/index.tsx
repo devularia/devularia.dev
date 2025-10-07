d4194ab277dce3d5f06c37990370a681f6db997b
devularia <devularia@proton.me>
2025-10-07 18:23:14 -0400

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Background } from '@/components/background'
+import Aurora from '@/components/bits/aurora'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -6,5 +7,8 @@ export const Route = createFileRoute('/')({
 })
 
 function RouteComponent() {
-  return <Background imageUrl='/backgrounds/gta/1.png'/>
+  return <div>
+    <Aurora/>
+    <Background imageUrl='/backgrounds/gta/1.png'/>
+  </div>
 }
