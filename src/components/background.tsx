33f71c058f3532c3a0e4e83812b7f7b6d3b8a656
devularia <devularia@proton.me>
2025-10-07 17:36:55 -0400

    Update background.tsx

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -6,7 +6,7 @@ export function Background({ imageUrl }: BackgroundProps) {
   return (
     <div className="absolute inset-0 -z-10">
       <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat saturate-0 opacity-20"
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat saturate-0 opacity-5"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
       <div className="absolute inset-0 bg-gradient-to-t from-background to-transparent" />
